b7f5187020427e784822ac0e4fa75a5e3e9c0836
Guilherme Krieck <116123096+GuiKrieck@users.noreply.github.com>
2024-05-08 12:09:40 -0300

    Criação do main.js
    criação do main.js com algumas funções para começar a preencher as informações, também foram feitos alguns ajustes no index.html e profile.json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,2 +1,7 @@
 
 
+async function fetchProfileData(){
+    const url = "https://guikrieck.github.io/Portfolio-DIO-TrilhaJavaScript/data/profile.json";
+    const fetching = await fetch(url);
+    return await fetching.json();
+}
---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+function updateProfileInfo(profileData){
+
+    const profilePhoto = document.querySelector('.header__image');
+    profilePhoto.src = profileData.photo;
+    profilePhoto.alt = profileData.name;
+
+    const profileName = document.querySelector('.header__name');
+    profileName.innerText = profileData.name;
+
+    const profileJob = document.querySelector('.information__job');
+    profileJob.innerText = profileData.job;
+
+    const profileLocation = document.querySelector('.information__location');
+    profileLocation.innerText = profileData.location;
+
+    const profilePhone = document.querySelector('.phone__link');
+    profilePhone.innerText = profileData.phone;
+    profilePhone.href = `tel:${profileData.phone}`;
+
+
+    const profileEmail = document.querySelector('.email__link');
+    profileEmail.innerText = profileData.email;
+    profileEmail.href = `mailto:${profileData.email}`;
+}
+
+function updateSoftSkills(profileData){
+
+    const softSkills = document.querySelector('.soft-skill__list');
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li><p>${skill}</p></li>`).join('')
+}
+
+function updateHardSkills(profileData){
+
+    const hardSkills = document.querySelector('.hard-skill__list');
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+}
+
+(async () => {
+
+    const profileData = await fetchProfileData();
+    updateProfileInfo(profileData);
+    updateSoftSkills(profileData);
+    updateHardSkills(profileData);
+})();

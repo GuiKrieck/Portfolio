cab0f13e97dfe1c1705af43e043a8077dc2543c3
Guilherme Krieck <116123096+GuiKrieck@users.noreply.github.com>
2024-05-09 10:46:33 -0300

    Projeto Finalizado
    Finalizado a primeira versão do portfólio

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,12 @@ function updateProfileInfo(profileData){
     const profileEmail = document.querySelector('.email__link');
     profileEmail.innerText = profileData.email;
     profileEmail.href = `mailto:${profileData.email}`;
+
+    const profileGithub = document.querySelector('.socials__github');
+    profileGithub.href = profileData.github;
+
+    const profileLinkedin = document.querySelector('.socials__linkedin');
+    profileLinkedin.href = profileData.linkedin; 
 }
 
 function updateSoftSkills(profileData){
@@ -59,7 +65,6 @@ function updateProjects(profileData){
     }).join("");
 }
 
-
 function updateLanguages(profileData){
     const languages = document.querySelector('.languages__list');
     languages.innerHTML = profileData.languages.map(language => {
@@ -72,6 +77,59 @@ function updateLanguages(profileData){
     }).join('');
 }
 
+function updateEducation(profileData){
+    const educationCourses = document.querySelector('.education__courses');
+    educationCourses.innerHTML = profileData.education.map((course, i, courses) => {
+        if ( courses[i+1] != undefined){
+            return `
+                <li>
+                    <h4>${course.course}</h4>
+                    <p>${course.period}</p>
+                    <p>${course.school}</p>
+                </li>
+                <div class="divider"></div>`
+        } else {
+            return`
+                <li>
+                    <h4>${course.course}</h4>
+                    <p>${course.period}</p>
+                    <p>${course.school}</p>
+                </li>`
+        }
+    }).join("");
+
+    const educationOne = document.querySelector('.one__list');
+    educationOne.innerHTML = profileData.one.map(course => updateCourses(course)).join("");
+
+    const educationDio = document.querySelector('.dio__list');
+    educationDio.innerHTML = profileData.dio.map(course => updateCourses(course)).join("");
+
+}
+
+function updateExperiences(profileData){
+    const experiences = document.querySelector('.experiences__list');
+    experiences.innerHTML = profileData.professionalExperience.map((experience, i, experiences) => {
+        if ( experiences[i+1] != undefined){
+            return `
+            <li>
+                <h3 class="experience__title">${experience.name}</h3>
+                <p class="experience__period">${experience.period}</p>
+                <p class="experience__company">${experience.company}</p>
+                <p class="experience__description">${experience.description}</p>
+            </li>
+            <div class="divider"></div>`
+        } else {
+            return`
+            <li>
+                <h3 class="experience__title">${experience.name}</h3>
+                <p class="experience__period">${experience.period}</p>
+                <p class="experience__company">${experience.company}</p>
+                <p class="experience__description">${experience.description}</p>
+            </li>`
+        }
+    }).join("");
+}
+
 
 (async () => {
 
@@ -81,4 +139,24 @@ function updateLanguages(profileData){
     updateHardSkills(profileData);
     updateProjects(profileData);
     updateLanguages(profileData);
+    updateEducation(profileData);
+    updateExperiences(profileData);
 })();
+
+function updateCourses(course){
+    if (course.isFinished){
+        return`
+            <li class="education__list-item">
+                <h4>${course.course}</h4>
+                <a href="${course.certificate}" target="_blank"><small>(certificado)</small></a>
+            </li>
+        `
+    } else{
+        return`
+            <li class="education__list-item">
+                <h4>${course.course}</h4>
+                <small>(cursando)</small>
+            </li>
+        `            
+    }
+}

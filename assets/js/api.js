b0760cd9b2f545d5786ca21b0a8dd6a439f7c33d
Guilherme Krieck <116123096+GuiKrieck@users.noreply.github.com>
2024-05-28 09:36:39 -0300

    Atualizando end Point da API

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData(){
-    const url = "https://guikrieck.github.io/Portfolio-DIO-TrilhaJavaScript/data/profile.json";
+    const url = "https://guikrieck.github.io/Portfolio/data/profile.json";
     const fetching = await fetch(url);
     return await fetching.json();
 }
